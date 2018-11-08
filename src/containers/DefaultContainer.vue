84c6b52f18cf743815035cd4095a8c24237bbbd5
yogaukha <yoga@natasolusi.co.id>
2018-11-08 21:17:41 +0700

    edit css

--- src/containers/DefaultContainer.vue
+++ src/containers/DefaultContainer.vue
@@ -3,7 +3,7 @@
     <AppHeader fixed>
       <SidebarToggler class="d-lg-none" display="md" mobile />
       <b-link class="navbar-brand" to="#">
-        <img class="navbar-brand-full" src="img/brand/logo.svg" width="89" height="25" alt="CoreUI Logo">
+        <img class="navbar-brand-full" src="img/fte-ui/logo-ui.png" alt="Logo UI">
         <img class="navbar-brand-minimized" src="img/brand/sygnet.svg" width="30" height="30" alt="CoreUI Logo">
       </b-link>
       <SidebarToggler class="d-md-down-none" display="lg" />
